2e1bd40efb209adaf5fe326113511c01dfbbf236
suppaduppax <mr.alvinaquino@gmail.com>
2022-01-27 23:55:37 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "fuhhh"
+
+log () {
+  echo "${1}"
+}
 
 wget https://gist.githubusercontent.com/suppaduppax/e7084b1a7b538c08b95a43b03d7430f7/raw/a15e6fcf399130626b4745ca5df28ef37726bfc8/setup_environment.sh
 chmod +x setup_environment.sh
@@ -12,7 +15,9 @@ directories=(
   "collections/" 
 )
 
+log "Creating directories..."
 for dir in "${directories[@]}"; do
+  log "  ${dir}"
   mkdir "${dir}" -p
 done
 
