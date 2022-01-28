5423056152f473c41b0935b602096cacac64f80e
suppaduppax <mr.alvinaquino@gmail.com>
2022-01-27 23:47:33 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "fuhhh"
+
 wget https://gist.githubusercontent.com/suppaduppax/e7084b1a7b538c08b95a43b03d7430f7/raw/a15e6fcf399130626b4745ca5df28ef37726bfc8/setup_environment.sh
 chmod +x setup_environment.sh
 
