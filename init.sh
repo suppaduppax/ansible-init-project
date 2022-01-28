159f2dd3b6195f5674a53c6a812c9e08aeced4db
suppaduppax <mr.alvinaquino@gmail.com>
2022-01-27 23:42:27 -0500

    Initial init.sh scrip

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wget https://gist.githubusercontent.com/suppaduppax/e7084b1a7b538c08b95a43b03d7430f7/raw/a15e6fcf399130626b4745ca5df28ef37726bfc8/setup_environment.sh
+chmod +x setup_environment.sh
+
+directories=(
+  "inventory",
+  "inventory/group_vars/all",
+  "inventory/host_vars/",
+  "roles/",
+  "collections/",
+)
+
+for dir in "${directories}"; do
+  mkdir "${dir}" -p
+done
+
+
