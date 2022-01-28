8d1a0615f96052acd4f0ea73a999d14c8ebb94b5
suppaduppax <mr.alvinaquino@gmail.com>
2022-01-27 23:59:03 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ log () {
   echo "${1}"
 }
 
-wget https://gist.githubusercontent.com/suppaduppax/e7084b1a7b538c08b95a43b03d7430f7/raw/a15e6fcf399130626b4745ca5df28ef37726bfc8/setup_environment.sh
+log "Fetching setup_environment.sh"
+curl -s https://gist.githubusercontent.com/suppaduppax/e7084b1a7b538c08b95a43b03d7430f7/raw/a15e6fcf399130626b4745ca5df28ef37726bfc8/setup_environment.sh > setup_environment.sh
 chmod +x setup_environment.sh
 
 directories=(
@@ -15,10 +16,18 @@ directories=(
   "collections/" 
 )
 
+files=(
+  "inventory/group_vars/all/vars.yml"
+)
+
 log "Creating directories..."
 for dir in "${directories[@]}"; do
   log "  ${dir}"
   mkdir "${dir}" -p
 done
 
-
+log "Creating files..."
+for file in "${files[@]}"; do
+  log "  ${file}"
+  mkdir "${file}" -p
+done
